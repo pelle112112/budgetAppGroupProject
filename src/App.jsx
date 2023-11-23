e96545bddd095d4e58a248b041e77119336a275d
CarstenJuhl <113057317+CarstenJuhl@users.noreply.github.com>
2023-11-23 13:25:45 +0100

    updated styling and stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
-import { useState } from 'react';
-import { useEffect } from 'react';
 import './styles/App.css';
 import BudgetApp from './components/BudgetApp';
-import { fetchData } from './api/api';
-import Expenses from './components/Expenses';
-import ExpenseForm from './components/ExpenseForm';
+
 
 function App() {
   
