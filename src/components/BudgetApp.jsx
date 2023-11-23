c4cf2f0650095d72a7ad0bae4189aff1829f2f47
MrJustMeDahl <nico.dahl93@gmail.com>
2023-11-23 13:03:08 +0100

    Implemented Income component with mock in budgetApp

--- src/components/BudgetApp.jsx
+++ src/components/BudgetApp.jsx
@@ -1,12 +1,17 @@
 import Expenses from "./Expenses";
 import Income from "./Income";
+import React, { useState} from 'react'
 
 function BudgetApp() {
+
+    //const[totalExpenses, setTotalExpenses] = useState[0];
+    const totalExpenses = 12000;
+
     return ( 
     <div>
         This is budgetApp
         <Expenses />
-        <Income />
+        <Income totalExpenses={totalExpenses} />
     </div>
     );
 }
